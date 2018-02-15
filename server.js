1fc5a34226fbbb1bb584c33a6772e3836107587d
Jerry Krusinski <jkruzr@gmail.com>
2018-02-15 12:06:07 -0600

    finshes server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const moment = require('moment-timezone');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.post('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const logPath = path.resolve(__dirname, `./${id}.log`);
+  const message = `${moment().tz('America/Chicago').toString()} - Checked In\n`;
 
-app.post('/', (req, res, next) => {
+  fs.appendFile(logPath, message, (err) => {
+    if (err) {
+      console.log(err);
+    }
 
+    res.send();
+  });
 });
 
 app.listen(9000, () => console.log('Server listening on port 9000'));
